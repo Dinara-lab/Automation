9573cafcb3b5547503fe42483d05efea63f71059
DinaraBucila <bddinara@gmail.com>
2022-01-13 17:35:34 +0200

    update JsonParserTest

--- src/test/java/parser/JsonParserTest.java
+++ src/test/java/parser/JsonParserTest.java
@@ -1,9 +1,7 @@
 package parser;
 import com.github.javafaker.Faker;
 import com.google.gson.Gson;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import shop.Cart;
 
 import java.io.*;
@@ -11,30 +9,30 @@ import java.io.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonParserTest {
-    private static Faker faker;
-    private static String object;
-    private static Gson gson;
     private static Cart cart;
+
 
     @BeforeAll
     static void beforeAll(){
-        faker = new Faker();
+        Faker faker = new Faker();
         System.out.println("before all");
         String name = faker.name().fullName();
         cart = new Cart(name);
-        gson = new Gson();
-        object = gson.toJson(cart);
+        JsonParser jsonParser = new JsonParser();
+        jsonParser.writeToFile(cart);
+
     }
+    @Test
+    void testIfExists(){
+        File file = new File("src/main/resources/" + cart.getCartName() + ".json");
+        assertTrue(file.exists(), "file does not exist");
+    }
+
 
     @Test
     public void writeToFile() throws IOException {
 
-        try (FileWriter writer = new FileWriter("src/main/resources/" + cart.getCartName() + ".json")) {
-            writer.write(object);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        Gson gson = new Gson();
         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/" + cart.getCartName() + ".json"));
         Cart newCart = gson.fromJson(reader.readLine(), Cart.class);
 
@@ -44,6 +42,29 @@ class JsonParserTest {
 
 
 
+}
+
+    @Test
+    public void readFromFile(){
+        File file = new File("src/main/resources/" + cart.getCartName() + ".json");
+        JsonParser jsonParser = new JsonParser();
+        Cart newCartObject = jsonParser.readFromFile(file);
+
+        assertAll(()-> assertEquals(newCartObject.getCartName(), cart.getCartName(), "object' names are not the same"),
+                () -> assertEquals(newCartObject.getRealItems(), cart.getRealItems(), "items are not the same"),
+                () ->  assertEquals(newCartObject.getTotalPrice(), cart.getTotalPrice(), "price is not the same"));
+
+        System.out.println(file.getName());
+
+
 
 }
+
+   @AfterAll
+   static void removeFile(){
+       File file = new File("src/main/resources/" + cart.getCartName() + ".json");
+       String name = file.getName();
+       file.delete();
+       System.out.println("file with the name " + name + "was deleted");
+   }
 }
